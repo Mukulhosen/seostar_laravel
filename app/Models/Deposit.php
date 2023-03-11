10988ac2962c4509ced44e5c69911b68878a9937
mdashikulislam <ashik.nwu@gmail.com>
2023-03-11 22:58:10 +0600

    chart data

--- app/Models/Deposit.php
+++ app/Models/Deposit.php
@@ -7,4 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Deposit extends Model
 {
     protected $table = 'deposits';
+    protected $fillable = ['user_id','address','amount','status','created','modified'];
 }
